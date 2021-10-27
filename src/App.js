134e4b9ee7c26db4fb61dad66981133559560f26
esther-v <esvalas@gmail.com>
2021-10-27 22:12:09 +0200

    display and update time song

--- src/App.js
+++ src/App.js
@@ -11,12 +11,18 @@ function App() {
   //state
   const [songs, setSongs] = useState(data())
   const [currentSong, setCurrentSong] = useState(songs[0])
+  const [isPlaying, setIsPlaying] = useState(false)
+
   return (
     <div className="App">
       <Song currentSong={currentSong}/>
-      <Player currentSong={currentSong}/>
+      <Player 
+        setIsPlaying={setIsPlaying} 
+        isPlaying={isPlaying} 
+        currentSong={currentSong}
+      />
     </div>
   );
-}
+} 
 
 export default App;
