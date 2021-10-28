09b09ed720e9a31bd4bdee296c3a5f7a4c84ab52
esther-v <esvalas@gmail.com>
2021-10-28 16:27:13 +0200

    pause button

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,6 +1,6 @@
 import React, { useRef, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faPlay, faAngleLeft, faAngleRight} from '@fortawesome/free-solid-svg-icons'
+import {faPlay, faAngleLeft, faAngleRight, faPause} from '@fortawesome/free-solid-svg-icons'
 
 const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
     //ref
@@ -32,8 +32,8 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
     }
     //state
     const [songInfo, setSongInfo] = useState({
-        currentTime: null,
-        duration: null
+        currentTime: 0,
+        duration: 0
     })
 
     return(
@@ -51,7 +51,12 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
             </div>
             <div className="play-control">
                 <FontAwesomeIcon className='skip-back' size="2x" icon={faAngleLeft}/>
-                <FontAwesomeIcon onClick={playSongHandler} className='play' size="2x" icon={faPlay}/> 
+                <FontAwesomeIcon 
+                    onClick={playSongHandler} 
+                    className='play' 
+                    size="2x" 
+                    icon={isPlaying ? faPause : faPlay}
+                /> 
                 <FontAwesomeIcon className='skip-forward' size="2x" icon={faAngleRight}/>
             </div>
             <audio 
