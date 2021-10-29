ae06ab4e59488808209398be57102bbb4e02bbca
esther-v <esvalas@gmail.com>
2021-10-29 20:34:43 +0200

    active song ok ▹

--- src/components/LibrarySong.js
+++ src/components/LibrarySong.js
@@ -1,9 +1,23 @@
 import React from 'react'
 
-const LibrarySong = ({ song, songs, setCurrentSong, id, audioRef, isPlaying }) => {
+const LibrarySong = ({ song, songs, setCurrentSong, id, audioRef, isPlaying, setSongs }) => {
     const songSelectHandler = () => {
         setCurrentSong(song)
-        
+        //add active state
+        const newSongs = songs.map((song) => {
+            if(song.id === id){
+                return{
+                    ...song,
+                    active: true
+                }
+            } else{
+                return{
+                    ...song,
+                    active: false
+                }
+            }
+        })
+        setSongs(newSongs)
         //check if the song is playing
         if(isPlaying){
             const playPromise = audioRef.current.play()
@@ -15,7 +29,7 @@ const LibrarySong = ({ song, songs, setCurrentSong, id, audioRef, isPlaying }) =
         }
     }
     return(
-        <div onClick={songSelectHandler} className="library-song">
+        <div onClick={songSelectHandler} className={`library-song ${song.active ? 'selected' : ""}`}>
             <img alt={song.name} src={song.cover} alt="drawing"></img>
             <div className="song-description">
                 <h3>{song.name}</h3>
